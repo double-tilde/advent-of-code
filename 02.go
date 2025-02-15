d4c7ea692f21bed9d540d8288b8a1476db8ea633
double-tilde <double-tilde@protonmail.com>
2025-02-15 18:31:52 +0000

    Working on part 2

--- 02.go
+++ 02.go
@@ -5,46 +5,88 @@ import (
 	"fmt"
 )
 
-func checkIncrementing(nums []int) bool {
+func checkIncrementing(nums, original []int) bool {
 	order := true
-	for i := range nums {
-		if i > 0 && nums[i-1] >= nums[i] {
-			return order == false
+	var badLevels []int
+
+	if len(nums) <= 1 {
+		return order
+	}
+
+	for i := 1; i < len(nums); i++ {
+		if nums[i-1] >= nums[i] || nums[i]-nums[i-1] > 3 {
+			order = false
+			badLevels = append(badLevels, i-1)
 		}
-		if i > 0 {
-			diff := nums[i] - nums[i-1]
-			if diff > 3 {
-				return order == false
+	}
+
+	if len(badLevels) > 0 {
+		for i := range badLevels {
+			newLevels := []int{}
+
+			for j := range original {
+				if original[j] != badLevels[i] {
+					newLevels = append(newLevels, original[j])
+				}
+			}
+
+			fmt.Println("Original:", original)
+			fmt.Println("New Levels:", newLevels)
+
+			if checkIncrementing(newLevels, original) {
+				return true
 			}
 		}
 	}
+
 	return order
 }
 
-func checkDecrementing(nums []int) bool {
+func checkDecrementing(nums, original []int) bool {
 	order := true
-	for i := range nums {
-		if i > 0 && nums[i-1] <= nums[i] {
-			return order == false
+	var badLevels []int
+
+	if len(nums) <= 1 {
+		return order
+	}
+
+	for i := 1; i < len(nums); i++ {
+		if nums[i-1] <= nums[i] || nums[i-1]-nums[i] > 3 {
+			order = false
+			badLevels = append(badLevels, i-1)
 		}
-		if i > 0 {
-			diff := nums[i-1] - nums[i]
-			if diff > 3 {
-				return order == false
+	}
+
+	if len(badLevels) > 0 {
+		for i := range badLevels {
+			newLevels := []int{}
+
+			for j := range original {
+				if original[j] != badLevels[i] {
+					newLevels = append(newLevels, original[j])
+				}
+			}
+
+			fmt.Println("Original:", original)
+			fmt.Println("New Levels:", newLevels)
+
+			if checkDecrementing(newLevels, original) {
+				return true
 			}
 		}
 	}
+
 	return order
 }
 
 func loop(list [][]int) int {
 	safeStrings := 0
 
-	for i := range list[:] {
-		if checkIncrementing(list[i]) {
-			safeStrings++
-		}
-		if checkDecrementing(list[i]) {
+	for _, nums := range list {
+		original := []int{}
+		original = append(original, nums...)
+
+		if checkIncrementing(nums, original) || checkDecrementing(nums, original) {
 			safeStrings++
 		}
 	}
@@ -52,7 +94,7 @@ func loop(list [][]int) int {
 	return safeStrings
 }
 
-func seconfProblem() {
+func secondProblem() {
 	listOfNums := utils.GetSpaceSeperatedNums("./assets/02-file.txt")
 
 	safe := loop(listOfNums)
