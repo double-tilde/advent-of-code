3f2f1578d5e235038d93870cb6f51bab6ad3363d
double-tilde <double-tilde@protonmail.com>
2025-02-17 11:31:36 +0000

    Tidying 1

--- 01.go
+++ 01.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 )
 
-// sort the slices into ascending order
 func sortList(list []int) []int {
 	// selection sort
 	for i := 0; i < len(list)-1; i++ {
@@ -21,7 +20,6 @@ func sortList(list []int) []int {
 	return list
 }
 
-// put one of the slices into a map
 func createMap(list []int) map[int]int {
 	m := make(map[int]int)
 
@@ -32,58 +30,32 @@ func createMap(list []int) map[int]int {
 	return m
 }
 
-// compare the other slice to the map (part 1)
-// func compare(m map[int]int, s []int) int {
-// 	sum := 0
-// 	for k, v := range m {
-// 		diff := 0
-//
-// 		if s[k] == v {
-// 			diff = 0
-// 		}
-//
-// 		if s[k] > v {
-// 			diff = s[k] - v
-// 		}
-//
-// 		if s[k] < v {
-// 			diff = v - s[k]
-// 		}
-//
-// 		sum = sum + diff
-// 	}
-//
-// 	return sum
-// }
-
-// see how many times a value in the left list appears in the right list
-// multiply that value by amount of appearances
-// the add up all of those values
+// See how many times a value in the left list appears in the right list
+// Multiply that value by amount of appearances then add up all of those values
 func compare(m map[int]int, s []int) int {
 	sum := 0
 	for i := range s {
-		multiplyBy := 0
+		appears := 0
 		for _, v := range m {
 			if s[i] == v {
-				multiplyBy++
+				appears++
 			}
 		}
-		multiplyBy = s[i] * multiplyBy
-		sum += multiplyBy
+		appears = s[i] * appears
+		sum += appears
 	}
 
 	return sum
 }
 
 func firstProblem() {
-	// Get the lists
 	l1 := utils.GetIntRecords("./assets/01-list1.csv")
 	l2 := utils.GetIntRecords("./assets/01-list2.csv")
 
 	sortList(l1)
 	sortList(l2)
 
-	// For part 2 of the question, the right list (list 2) needs to be in the map
+	// For part 2 of the question, list 2 needs to be in the map
 	m := createMap(l2)
 
 	res := compare(m, l1)
