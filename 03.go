f7bb6bc1691e88da5cfdf5fbd050d838b272603d
double-tilde <double-tilde@protonmail.com>
2025-02-21 18:25:38 +0000

    Day 3 part 2

--- 03.go
+++ 03.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"regexp"
+	"sort"
 	"strconv"
 )
 
@@ -17,14 +18,58 @@ func compileRegex(p string) *regexp.Regexp {
 	return r
 }
 
-func findMulFuncs(input string) []string {
-	var mulFuncs []string
+type match struct {
+	text  string
+	index int
+}
 
-	r := compileRegex(`mul\((\d{1,3}),(\d{1,3})\)`)
+func findValidFuncs(input string) []string {
+	regexes := []*regexp.Regexp{
+		regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`),
+		regexp.MustCompile(`do\(\)`),
+		regexp.MustCompile(`don't\(\)`),
+	}
 
-	mulFuncs = r.FindAllString(input[:], -1)
+	results := make(chan []match, len(regexes))
 
-	return mulFuncs
+	for _, re := range regexes {
+		go func(re *regexp.Regexp) {
+			var matches []match
+			for _, loc := range re.FindAllStringIndex(input, -1) {
+				matches = append(matches, match{text: input[loc[0]:loc[1]], index: loc[0]})
+			}
+			results <- matches
+		}(re)
+	}
+
+	var allMatches []match
+	for i := 0; i < len(regexes); i++ {
+		allMatches = append(allMatches, <-results...)
+	}
+	close(results)
+
+	sort.Slice(allMatches, func(i, j int) bool {
+		return allMatches[i].index < allMatches[j].index
+	})
+
+	var validFuncs []string
+	do := true
+
+	for _, match := range allMatches {
+		if match.text == "do()" {
+			do = true
+		}
+		if match.text == "don't()" {
+			do = false
+		}
+
+		if do {
+			validFuncs = append(validFuncs, match.text)
+		}
+
+	}
+
+	return validFuncs
 }
 
 func getNumberPairs(strs []string) [][]int {
@@ -68,8 +113,9 @@ func calculateProduct(numPairs [][]int) int {
 func thirdProblem() {
 	input := utils.GetOneString("./assets/03-file.txt")
 
-	mulFuncs := findMulFuncs(input)
-	numberPairs := getNumberPairs(mulFuncs)
+	validFuncs := findValidFuncs(input)
+
+	numberPairs := getNumberPairs(validFuncs)
 	res := calculateProduct(numberPairs)
 
 	fmt.Println("Problem 3:", res)
