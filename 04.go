61209e099dc1c6aaf95af5806b899e12065086e6
double-tilde <double-tilde@protonmail.com>
2025-03-04 13:37:27 +0000

    Ugly day 4 part 2 - needs improving

--- 04.go
+++ 04.go
@@ -44,40 +44,6 @@ func isInBounds(matrix [][]string, row, col int) bool {
 
 func linearWordSearch(
 	matrix [][]string,
-	uiMatrix string,
-	si SearchInput,
-	curWord map[string][]int,
-) string {
-	for j := range matrix {
-		for k := range matrix[j] {
-			curChar := string(matrix[j][k])
-
-			val, exists := curWord[curChar]
-			if exists && len(curWord) == len(si.word) && val[0] == j &&
-				val[1] == k {
-				uiMatrix += ui.StringColor(curChar, ui.GreenBgBlackText)
-			} else if exists && val[0] == j && val[1] == k {
-				uiMatrix += ui.StringColor(curChar, ui.YellowBgBlackText)
-			} else {
-				uiMatrix += curChar
-			}
-		}
-		uiMatrix += "\n"
-	}
-	return uiMatrix
-}
-
-func shapedWordSearch(
-	matrix [][]string,
-	uiMatrix string,
-	si SearchInput,
-	curWord map[string][]int,
-) string {
-	return uiMatrix
-}
-
-func searchWord(
-	matrix [][]string,
 	row, col int,
 	si SearchInput,
 	dir []int,
@@ -87,20 +53,32 @@ func searchWord(
 	curWord := make(map[string][]int)
 
 	for i := 0; i < len(si.word); i++ {
+
+		var uiMatrix string
+
 		if !isInBounds(matrix, row, col) || matrix[row][col] != string(si.word[i]) {
 			return false
 		}
 
-		var uiMatrix string
-
 		if matrix[row][col] == string(si.word[i]) {
-			curWord[string(matrix[row][col])] = []int{row, col}
+			curWord[matrix[row][col]] = []int{row, col}
 		}
 
-		if si.linear {
-			uiMatrix = linearWordSearch(matrix, uiMatrix, si, curWord)
-		} else {
-			uiMatrix = shapedWordSearch(matrix, uiMatrix, si, curWord)
+		for j := range matrix {
+			for k := range matrix[j] {
+				curChar := matrix[j][k]
+
+				val, exists := curWord[curChar]
+				if exists && len(curWord) == len(si.word) && val[0] == j &&
+					val[1] == k {
+					uiMatrix += ui.StringColor(curChar, ui.GreenBgBlackText)
+				} else if exists && val[0] == j && val[1] == k {
+					uiMatrix += ui.StringColor(curChar, ui.YellowBgBlackText)
+				} else {
+					uiMatrix += curChar
+				}
+			}
+			uiMatrix += "\n"
 		}
 
 		ui.Create(uiMatrix, sigChan, ticker)
@@ -112,13 +90,88 @@ func searchWord(
 	return true
 }
 
+func shapedWordSearch(
+	matrix [][]string,
+	row, col int,
+	si SearchInput,
+	sigChan chan os.Signal,
+	ticker *time.Ticker,
+) bool {
+	dirs := si.directions
+
+	curWord := make(map[string][]int)
+	var cRow, cCol, oppRow, oppCol int
+
+	for _, dir := range dirs {
+		dRow := dir[0]
+		dCol := dir[1]
+
+		if !isInBounds(matrix, row, col) {
+			return false
+		}
+
+		_, exists := curWord["A"]
+
+		if dRow == 0 && dCol == 0 && matrix[row][col] == "A" {
+			cRow = row
+			cCol = col
+			curWord[matrix[cRow][cCol]] = []int{cRow, cCol}
+		}
+
+		if !isInBounds(matrix, cRow+dRow, cCol+dCol) {
+			return false
+		}
+
+		if exists && dRow == 1 && dCol == 1 && (matrix[cRow+dRow][cCol+dCol] == "M" ||
+			matrix[cRow+dRow][cCol+dCol] == "S") {
+			oppRow = cRow + dRow
+			oppCol = cCol + dCol
+			curWord[matrix[oppRow][oppCol]+"11"] = []int{oppRow, oppCol}
+		}
+
+		if exists && dRow == -1 && dCol == -1 &&
+			matrix[cRow+dRow][cCol+dCol] != matrix[oppRow][oppCol] &&
+			(matrix[cRow+dRow][cCol+dCol] == "M" ||
+				matrix[cRow+dRow][cCol+dCol] == "S") {
+			curWord[matrix[cRow+dRow][cCol+dCol]+"-1-1"] = []int{cRow + dRow, cCol + dCol}
+		}
+
+		if exists && dRow == -1 && dCol == 1 && (matrix[cRow+dRow][cCol+dCol] == "M" ||
+			matrix[cRow+dRow][cCol+dCol] == "S") {
+			oppRow = cRow + dRow
+			oppCol = cCol + dCol
+			curWord[matrix[oppRow][oppCol]+"-11"] = []int{oppRow, oppCol}
+		}
+
+		if exists && dRow == 1 && dCol == -1 &&
+			matrix[cRow+dRow][cCol+dCol] != matrix[oppRow][oppCol] &&
+			(matrix[cRow+dRow][cCol+dCol] == "M" ||
+				matrix[cRow+dRow][cCol+dCol] == "S") {
+			curWord[matrix[cRow+dRow][cCol+dCol]+"1-1"] = []int{cRow + dRow, cCol + dCol}
+		}
+
+	}
+
+	if len(curWord) != 5 {
+		return false
+	}
+
+	return true
+}
+
 func getCount(matrix [][]string, si SearchInput, sigChan chan os.Signal, ticker *time.Ticker) int {
 	count := 0
 
 	for row := range matrix {
 		for col := range matrix[row] {
-			for _, dir := range si.directions {
-				if searchWord(matrix, row, col, si, dir, sigChan, ticker) {
+			if si.linear {
+				for _, dir := range si.directions {
+					if linearWordSearch(matrix, row, col, si, dir, sigChan, ticker) {
+						count++
+					}
+				}
+			} else {
+				if shapedWordSearch(matrix, row, col, si, sigChan, ticker) {
 					count++
 				}
 			}
@@ -133,16 +186,19 @@ func fourthProblem() {
 
 	matrix := createMatrix(input)
 
-	si1 := createSearchInput(
-		"XMAS",
-		[][]int{{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}},
-		true,
-	)
+	// si1 := createSearchInput(
+	// 	"XMAS",
+	// 	[][]int{{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}},
+	// 	true,
+	// )
+
+	si2 := createSearchInput("MAS", [][]int{{0, 0}, {1, 1}, {-1, -1}, {-1, 1}, {1, -1}}, false)
+	// si2 := createSearchInput("MAS", [][]int{{0, 0}, {1, 1}, {-1, -1}}, false)
 
 	sigChan, ticker := ui.Setup(10)
 	defer ticker.Stop()
 
-	res := getCount(matrix, si1, sigChan, ticker)
+	res := getCount(matrix, si2, sigChan, ticker)
 
 	fmt.Println("Fourth Problem:", res)
 	fmt.Println(ui.ShowCursor)
