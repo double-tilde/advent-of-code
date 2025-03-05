a8428f17f74b96b5050d26e92e6d79327860fadc
double-tilde <double-tilde@protonmail.com>
2025-03-05 12:33:47 +0000

    Fix note

--- 04.go
+++ 04.go
@@ -11,7 +11,7 @@ import (
 type SearchInput struct {
 	Word       string
 	Directions [][]int
-	Linear     bool
+	isLinear   bool
 }
 
 type WordPosition struct {
@@ -131,56 +131,61 @@ func shapedWordSearch(
 			return false
 		}
 
-		if exists && dRow == 1 && dCol == 1 && (matrix[cRow+dRow][cCol+dCol] == "M" ||
-			matrix[cRow+dRow][cCol+dCol] == "S") {
-			oppRow = cRow + dRow
-			oppCol = cCol + dCol
-			curWord = append(
-				curWord,
-				WordPosition{Char: matrix[oppRow][oppCol], Row: oppRow, Col: oppCol},
-			)
-		}
-
-		if exists && dRow == -1 && dCol == -1 &&
-			matrix[cRow+dRow][cCol+dCol] != matrix[oppRow][oppCol] &&
-			(matrix[cRow+dRow][cCol+dCol] == "M" ||
+		if exists {
+			if dRow == 1 && dCol == 1 && (matrix[cRow+dRow][cCol+dCol] == "M" ||
 				matrix[cRow+dRow][cCol+dCol] == "S") {
-			curWord = append(
-				curWord,
-				WordPosition{
-					Char: matrix[cRow+dRow][cCol+dCol],
-					Row:  cRow + dRow,
-					Col:  cCol + dCol,
-				},
-			)
-		}
+				oppRow = cRow + dRow
+				oppCol = cCol + dCol
+				curWord = append(
+					curWord,
+					WordPosition{Char: matrix[oppRow][oppCol], Row: oppRow, Col: oppCol},
+				)
+			}
 
-		if exists && dRow == -1 && dCol == 1 && (matrix[cRow+dRow][cCol+dCol] == "M" ||
-			matrix[cRow+dRow][cCol+dCol] == "S") {
-			oppRow = cRow + dRow
-			oppCol = cCol + dCol
-			curWord = append(
-				curWord,
-				WordPosition{Char: matrix[oppRow][oppCol], Row: oppRow, Col: oppCol},
-			)
-		}
+			if dRow == -1 && dCol == -1 &&
+				matrix[cRow+dRow][cCol+dCol] != matrix[oppRow][oppCol] &&
+				(matrix[cRow+dRow][cCol+dCol] == "M" ||
+					matrix[cRow+dRow][cCol+dCol] == "S") {
+				curWord = append(
+					curWord,
+					WordPosition{
+						Char: matrix[cRow+dRow][cCol+dCol],
+						Row:  cRow + dRow,
+						Col:  cCol + dCol,
+					},
+				)
+			}
 
-		if exists && dRow == 1 && dCol == -1 &&
-			matrix[cRow+dRow][cCol+dCol] != matrix[oppRow][oppCol] &&
-			(matrix[cRow+dRow][cCol+dCol] == "M" ||
+			// FIX: these are in a different order to the slice, so it breaks
+			// Make the slice sorted first?
+			if dRow == 1 && dCol == -1 && (matrix[cRow+dRow][cCol+dCol] == "M" ||
 				matrix[cRow+dRow][cCol+dCol] == "S") {
-			curWord = append(
-				curWord,
-				WordPosition{
-					Char: matrix[cRow+dRow][cCol+dCol],
-					Row:  cRow + dRow,
-					Col:  cCol + dCol,
-				},
-			)
+				oppRow = cRow + dRow
+				oppCol = cCol + dCol
+				curWord = append(
+					curWord,
+					WordPosition{Char: matrix[oppRow][oppCol], Row: oppRow, Col: oppCol},
+				)
+			}
+
+			if dRow == -1 && dCol == 1 &&
+				matrix[cRow+dRow][cCol+dCol] != matrix[oppRow][oppCol] &&
+				(matrix[cRow+dRow][cCol+dCol] == "M" ||
+					matrix[cRow+dRow][cCol+dCol] == "S") {
+				curWord = append(
+					curWord,
+					WordPosition{
+						Char: matrix[cRow+dRow][cCol+dCol],
+						Row:  cRow + dRow,
+						Col:  cCol + dCol,
+					},
+				)
+			}
 		}
 
 		uiMatrix := createUI(matrix, len(dirs), curWord)
-		ui.Create(uiMatrix, sigChan, ticker)
+		fmt.Println(uiMatrix)
+		// ui.Create(uiMatrix, sigChan, ticker)
 	}
 
 	return len(curWord) == 5
@@ -191,7 +196,7 @@ func getCount(matrix [][]string, si SearchInput, sigChan chan os.Signal, ticker 
 
 	for row := range matrix {
 		for col := range matrix[row] {
-			if si.Linear {
+			if si.isLinear {
 				for _, dir := range si.Directions {
 					if linearWordSearch(matrix, row, col, si, dir, sigChan, ticker) {
 						count++
@@ -215,21 +220,27 @@ func fourthProblem() {
 
 	// si1 := SearchInput{
 	// 	Word:       "XMAS",
+	// 	// b, br, r, tr, t, tl, l, bl
 	// 	Directions: [][]int{{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}},
-	// 	Linear:     true,
+	// 	isLinear:     true,
 	// }
 
 	si2 := SearchInput{
-		Word:       "MAS",
+		Word: "MAS",
+		// c, br, tl, bl, tr
+		// Directions: [][]int{{0, 0}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1}},
 		Directions: [][]int{{0, 0}, {1, 1}, {-1, -1}, {-1, 1}, {1, -1}},
-		Linear:     false,
+		isLinear:   false,
 	}
 
 	sigChan, ticker := ui.Setup(10)
 	defer ticker.Stop()
 
-	res := getCount(matrix, si2, sigChan, ticker)
+	// res1 := getCount(matrix, si1, sigChan, ticker)
 
-	fmt.Println("Fourth Problem:", res)
+	res2 := getCount(matrix, si2, sigChan, ticker)
+	// fmt.Println("Fourth Problem:", res1, res2)
+	fmt.Println("Fourth Problem:", res2)
+
 	fmt.Println(ui.ShowCursor)
 }
