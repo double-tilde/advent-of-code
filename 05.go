6398d2431df659e8d8714e9e272333048a6f790d
double-tilde <double-tilde@protonmail.com>
2025-03-13 20:45:09 +0000

    Finished day 5 part 1

--- 05.go
+++ 05.go
@@ -9,12 +9,16 @@ func getSlicePairs(s []int) [][]int {
 	var sm [][]int
 	for i := 0; i < len(s); i++ {
 		for j := 0; j < len(s); j++ {
-			var sp []int
 			if j == i {
 				continue
 			}
-			sp = append(sp, s[i], s[j])
-			sm = append(sm, sp)
+
+			lhs, rhs := s[i], s[j]
+			if j < i {
+				lhs, rhs = rhs, lhs
+			}
+
+			sm = append(sm, []int{lhs, rhs})
 		}
 	}
 	return sm
@@ -22,12 +26,9 @@ func getSlicePairs(s []int) [][]int {
 
 func compareSlices(rulesSet [][]int, pages []int) bool {
 	for i := range rulesSet {
-		fmt.Println("comparing", rulesSet[i][0], rulesSet[i][1], "with", pages[0], pages[1])
 		if rulesSet[i][0] == pages[1] && rulesSet[i][1] == pages[0] {
-			fmt.Println("wrong order", rulesSet[i][0], rulesSet[i][1], "see", pages[0], pages[1])
 			return false
 		}
-		fmt.Println("right order", rulesSet[i][0], rulesSet[i][1], "see", pages[0], pages[1])
 	}
 	return true
 }
@@ -52,19 +53,13 @@ func FifthProblem() {
 		if ok {
 			goodPages = append(goodPages, pages)
 		}
-		fmt.Println(goodPages)
 	}
 
-	// 1. For the rules, create a [][]int with the first num as [i][0] and the second as [i][1]
-	//    The pipe can be used as the deliminator
-	// 2. For the page numbers, also create a [][]int, with each list as [i][i]
-	// 3. Starting with the first set of page numbers, start with [0][0] and [0][1] and put them in a []int
-	//    Check the positions. If they are in the right order against the rules, they should appear in the
-	//    same positions that they are in currently in their comparrison slice
-	// 4. If they are not matching, return early as the page numbers are in the wrong order, if they are,
-	//    then move to compare [0][0] and [0][2], and so on
-	// 5. If the slices are correct, append them to a [][]int of correct slices
-	// 6. For each of the correct slices, find the int in the middle position and add them up
+	var res int
+	for _, pages := range goodPages {
+		mp := 0 + len(pages)/2
+		res += pages[mp]
+	}
 
-	fmt.Println("Fifth Problem:")
+	fmt.Println("Fifth Problem:", res)
 }
