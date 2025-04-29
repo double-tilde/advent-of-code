17fc4ae7cff453e89da5865b9d270e7cd9630b5f
double-tilde <double-tilde@protonmail.com>
2025-04-29 13:40:33 +0100

    Working on problem 1

--- 01.py
+++ 01.py
@@ -1,6 +1,23 @@
-x = 4
-y = 10
-z = 2
+import csv
 
-print("Hello, world!")
-print(x + y)
+
+def get_int_list_from_csv(file):
+    list = []
+    with open(file, mode="r") as f:
+        csv_reader = csv.reader(f)
+        for row in csv_reader:
+            for col in row:
+                list.append(col)
+
+    return list
+
+
+def main():
+    # get lists
+    li1 = get_int_list_from_csv("./assets/list1.csv")
+    li2 = get_int_list_from_csv("./assets/list2.csv")
+    print(li1, li2)
+
+
+if __name__ == "__main__":
+    main()
